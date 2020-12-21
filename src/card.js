6dae915dca93ce17012fd2a182eeb726884dea11
ArkaresK <calvin.pj@sfr.fr>
2020-12-21 16:54:34 +0100

    update icons

---
+++ src/card.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import akali from './champion-akali-bloodmoon-splash.jpg'
+export default class Card extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+      }
+    clicked(Image){
+      this.props.click(Image)
+    }
+    render(){
+      return (
+        <div className={"card" + (!this.props.close ? ' opened' : '') + (this.props.complete ? ' matched' : '')} onClick={() => this.clicked(this.props.Image)}>
+          <div className="front">
+          </div>
+          <div className="back">
+            <img src={/*akali */"https://raw.githubusercontent.com/samiheikki/javascript-guessing-game/master/static/logos/" 
+            + this.props.Image + ".png"} alt={this.props.Image}/>
+          </div>
+        </div>
+      )
+    }
+  }
